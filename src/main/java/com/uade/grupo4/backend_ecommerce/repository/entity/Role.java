b674bf10160aff85b8a0af28de086b4da43f1ddf
frarenas <francisco.arenas@mercadolibre.com>
2024-09-12 17:17:49 -0300

    Products data

---
+++ src/main/java/com/uade/grupo4/backend_ecommerce/repository/entity/Role.java
@@ -0,0 +1,6 @@
+package com.uade.grupo4.backend_ecommerce.repository.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- src/main/java/com/uade/grupo4/backend_ecommerce/repository/entity/User.java
+++ src/main/java/com/uade/grupo4/backend_ecommerce/repository/entity/User.java
@@ -3,14 +3,19 @@ package com.uade.grupo4.backend_ecommerce.repository.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Data
 @NoArgsConstructor
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +27,8 @@ public class User {
     private LocalDate birthDate;
     private String firstName;
     private String lastName;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public User(int id, String username, String email, String password, LocalDate birthDate, String firstName, String lastName) {
         this.id = id;
@@ -31,5 +38,15 @@ public class User {
         this.birthDate = birthDate;
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
     }
 }
