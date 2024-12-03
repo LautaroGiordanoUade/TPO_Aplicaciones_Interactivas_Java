e407163ee0e56926a0687a56988ac9b04c50d595
Fedegewis <80460038+Fedegewis@users.noreply.github.com>
2024-12-02 22:52:58 -0300

    Carrito compras (#36)
    * Cambios en el funcionamiento y espero que correcto
    
    * Cambios en el funcionamiento y espero que correcto
    
    * Cambios en el funcionamiento del cart y agregacion del purchase-history
    
    * Cambios en el funcionamiento del cart
    
    * Cambios en el funcionamiento del cart

--- src/main/java/com/uade/grupo4/backend_ecommerce/repository/CartRepository.java
+++ src/main/java/com/uade/grupo4/backend_ecommerce/repository/CartRepository.java
@@ -5,10 +5,11 @@ import com.uade.grupo4.backend_ecommerce.repository.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
      Optional<Cart> findById(Long id);
      Optional<Cart> findByUserAndCheckoutDate(User user, Date checkoutDate);
-     Optional<Cart> findByUser(User user);
+     List<Cart> findByUser(User user);
 }
